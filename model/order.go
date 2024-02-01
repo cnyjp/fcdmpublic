1127d986c284086b7495afa90fcf1566f2500cbc
yjp <cn_yjp@163.com>
2024-02-01 11:18:32 +0800

    add ImageName to BackupResponse

--- model/order.go
+++ model/order.go
@@ -1,24 +1,28 @@
 package model
 
-/**
-  The struct used by provider backup command.
-  When the provider run backup command and return a special code, will use the BackupResponse struct to unmarshal
-  the last line printed by the provider.
-  If the unmarshal error, then do not use the returned value to set the next step message.
+/*
+*
+
+	The struct used by provider backup command.
+	When the provider run backup command and return a special code, will use the BackupResponse struct to unmarshal
+	the last line printed by the provider.
+	If the unmarshal error, then do not use the returned value to set the next step message.
 */
 type BackupResponse struct {
-	InitMessage       string `json:"initMessage,omitempty"`       //the init jobstep's job returned message.
+	InitMessage       string `json:"initMessage,omitempty"`       //the init jobStep job returned message.
 	ProtectedDataSize int64  `json:"protectedDataSize,omitempty"` //the backup job protected data size.
-
+	ImageName         string `json:"imageName,omitempty"`         //the provider named image name.
 	//sometimes we need the backup image configure some option for the mount or restore action, use the configs return
 	//and save then in the image options. which will cover the option of the application's options.
 	Configs []ConfigConfig `json:"configs,omitempty"`
 }
 
-/**
-   The struct used by provider list_app command.
-   Whent the provider run list_app command and return a zero code, will use the struct to unmarshal the last line
-  printed by the provider.
+/*
+*
+
+	 The struct used by provider list_app command.
+	 Whent the provider run list_app command and return a zero code, will use the struct to unmarshal the last line
+	printed by the provider.
 */
 type ListAppResponse struct {
 	HostId             string            `json:"hostId,omitempty"`
